c019a78f7e4df2a5ebe9e28b6310c4d147daedae
gyullo18 <99640002+dprnd@users.noreply.github.com>
2022-07-14 16:33:56 +0900

    0714
    Warning, Status, SettingMenu

--- HungryJimmy/Assets/01.Scripts/Item/ItemPickUp.cs
+++ HungryJimmy/Assets/01.Scripts/Item/ItemPickUp.cs
@@ -5,13 +5,4 @@ public class ItemPickUp : MonoBehaviour
     //
     public Item item;
 
-    // private void OnTriggerEnter(Collider other)
-    // {
-    //     if (other.tag == "Player")
-    //     {
-    //         Debug.Log("아이템 획득");
-    //         // gameObject.SetActive(false);
-            
-    //     }
-    // }
 }
